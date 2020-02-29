99d2576e81ab2c0bcf40925aea61840224ecee7d
Robin <conlawman@gmail.com>
2020-02-29 15:21:48 +0000

    Added CRUD

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Customer(models.Model):
 	name = models.CharField(max_length=200, null=True)
@@ -44,3 +43,6 @@ class Order(models.Model):
 	product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True)
 	status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+	def __str__(self):
+		return self.product.name
